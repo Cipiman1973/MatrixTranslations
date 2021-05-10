0571ffdca3f9fef756072da49537b2d6472dbc6a
Cipiman1973 <62877816+Cipiman1973@users.noreply.github.com>
2021-05-10 14:18:05 +0200

    Update Shader.java

--- src/main/java/cz/cipisek/game/Shader.java
+++ src/main/java/cz/cipisek/game/Shader.java
@@ -3,27 +3,18 @@ package cz.cipisek.game;
 import org.lwjgl.opengl.GL33;
 
 public class Shader {
-    private static final String vertexShader = "#version 330 core \n" +
+    private static final String vertexShaderSource = "#version 330 core\n" +
             "layout (location = 0) in vec3 aPos;\n" +
-            "layout (location = 1) in vec4 vertexColors;\n" +
-
-            "uniform mat4 matrix;" +
-
-            "out vec4 myColors;\n" +
-
             "void main()\n" +
             "{\n" +
-            "gl_Position = matrix *  vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" +
-            "myColors = vertexColors;\n" +
+            " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" +
             "}";
 
-
-    private static final String fragmentShader = "#version 330 core \n" +
+    private static final String fragmentShaderSource = "#version 330 core\n" +
             "out vec4 FragColor;\n" +
-            "in vec4 myColors;\n" +
             "void main()\n" +
             "{\n" +
-            "   FragColor = myColors;\n" +
+            "FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n" +
             "}\n";
 
     public static int vertexShaderId;
@@ -34,12 +25,12 @@ public class Shader {
         vertexShaderId = GL33.glCreateShader(GL33.GL_VERTEX_SHADER);
         fragmentShaderId = GL33.glCreateShader(GL33.GL_FRAGMENT_SHADER);
 
-        GL33.glShaderSource(vertexShaderId, vertexShader);
+        GL33.glShaderSource(vertexShaderId, vertexShaderSource);
         GL33.glCompileShader(vertexShaderId);
 
         System.out.println(GL33.glGetShaderInfoLog(vertexShaderId));
 
-        GL33.glShaderSource(fragmentShaderId, fragmentShader);
+        GL33.glShaderSource(fragmentShaderId, fragmentShaderSource);
         GL33.glCompileShader(fragmentShaderId);
 
         System.out.println(GL33.glGetShaderInfoLog(fragmentShaderId));
@@ -53,6 +44,7 @@ public class Shader {
 
         GL33.glDeleteShader(vertexShaderId);
         GL33.glDeleteShader(fragmentShaderId);
+        GL33.glUseProgram(shaderProgramId);
     }
 
 }
